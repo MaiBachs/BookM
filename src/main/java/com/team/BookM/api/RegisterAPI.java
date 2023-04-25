db7d352ce1c0d8d9befa09d61127d1a81b3ba7e5
MaiBachs <maibach699@gmail.com>
2023-04-25 09:59:26 +0700

    edit

--- src/main/java/com/team/BookM/api/RegisterAPI.java
+++ src/main/java/com/team/BookM/api/RegisterAPI.java
@@ -5,6 +5,7 @@ import com.team.BookM.entity.User;
 import com.team.BookM.repository.UserDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class RegisterAPI {
     @Autowired
     private PasswordEncoder passwordEncoder;
